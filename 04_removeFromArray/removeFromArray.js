929269dfedcaad6df7fc140610bedb406d22ee10
Akarshan <107797503+aku47z@users.noreply.github.com>
2024-04-28 22:30:00 +0545

    Ex 01-07 done ✅

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...args) {
+    let i = 0;
+    while (i < arr.length) {
+        for (let j = 0; j < args.length; j++) {
+            if (args[j] === arr[i]) {
+                arr.splice(i, 1);
+                i--;
+            }
+        }
+        i++;
+    }
+    return arr;
 };
 
 // Do not edit below this line
